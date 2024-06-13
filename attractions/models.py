34bf25af5a5932ca04016f6c6abd4fecdb8b4ec6
DespinaFostiropoulou <dfostir@hotmail.com>
2024-06-13 23:22:02 +0300

    Second Commit

--- attractions/models.py
+++ attractions/models.py
@@ -21,6 +21,14 @@ class Attractions(models.Model):
     city = models.CharField(max_length=100)
     linenos = models.BooleanField(default=False)
     owner = models.ForeignKey('auth.User', related_name='attractions', on_delete=models.CASCADE, null=True)
+    urls = models.URLField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.url:
+            base_url = "https://www.google.com/maps/search/?api=1&query="
+            query = f"{self.title}, {self.city}, {self.country}"
+            self.url = base_url + query.replace("","+")
+        super(Attractions, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['created']
